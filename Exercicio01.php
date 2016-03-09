46c27ce376d76e5654fae4963062b408e4677dee
Murilo Miller Pereira Silva <muriloheavymetal@gmail.com>
2016-03-09 16:15:14 -0300

    Adicionados novos arquivos e exemplos
    Foram adicionados arquivos atualizados mais exemplos demonstrativos em planilha como requisitado.

--- Exercicio01.php
+++ Exercicio01.php
@@ -1,1 +1,46 @@
 <?php
+print "Digite a inicial do sexo do cliente: "; 				//saida de dados
+$sex = trim(fgets(STDIN));									//entrada de dados
+print "Digite a quantidade de cervejas consumidas: ";		//saida de dados
+$qtcerv = trim(fgets(STDIN));								//entrada de dados
+print "Digite a quantidade de refrigerantes consumidos: ";	//saida de dados
+$qtref = trim(fgets(STDIN));								//entrada de dados
+print "Digite a quantidade de espetinhos consumidos: ";		//saida de dados
+$qtesp = trim(fgets(STDIN));								//entrada de dados
+$cantores = 3.00;											//atribuição
+$tcerv = $qtcerv * 2.50;									//atribuição
+$trefri = $qtref * 2.00;									//atribuição
+$tesp = $qtesp * 4.00;										//atribuição
+$cons = $tcerv + $trefri + $tesp;							//atribuição
+
+print "---------------------------------------------------";
+
+print "\nTotalizadores:";										//saida de dados
+
+print "\n---------------------------------------------------";
+
+if ($sex=="f" || $sex=="F") {				
+	print "\nR$ 8,00  (Entrada femenina)";					//se for mulher cobra 8 e armazena valor na variavel ent
+	$ent = 8.00;
+}
+else {				
+	print "\nR$ 10,00 (Entrada masculina)";					//se for mulher cobra 8 e armazena valor na variavel ent
+	$ent = 10.00;
+}
+if ($cons > 15.00) {
+	print "\nR$ 0,00  (Couvert artístico)";							//se consumir mais de 15 nao paga couver e armazena 0 na  variavel couv
+	$couv = 0.00;
+}
+else {
+	print "\nR$  ".$cantores." (Couvert artístico)";						//se consumir 15 ou menos imprimi variavel cantores e adiciona o valor na variavel couv
+	$couv = $cantores;
+}
+$subtotal = $ent + $cons + $couv;							//atribuição
+$totalcpec = ($subtotal/100)*10 + $subtotal;				//atribuiçãos
+//formato decimal
+$subtotal = number_format($subtotal, 2, ',', ' ');
+$totalcpec = number_format($totalcpec, 2, ',', ' ');
+print "\nR$ ".$cons ."  (Consumação)";						//saida de dados
+print "\nR$ ".$subtotal." (subtotal sem 10%)";				//saida de dados
+print "\n---------------------------------------------------";//saida de dados
+print "\nR$ ".$totalcpec." (total)";							//saida de dados
--- Exercicio02.php
+++ Exercicio02.php
@@ -1,55 +1,53 @@
 <?php
-print "Digite o valor dos rendimentos bancários: ";
-$rendb = trim(fgets(STDIN));
-$tribrendb  = ($rendb * 0.2); 																//Trib 20% sobre rendimentos bancarios.
-print "Digite o valor dos rendimentos de salário ou serviços: ";
-$rendss = trim(fgets(STDIN));
+print "Digite o valor dos rendimentos bancários: ";											//saida de dados
+$rendb = trim(fgets(STDIN));																//entrada de dados
+$tribrendb  = ($rendb * 0.2); 																//atribuição Trib 20% sobre rendimentos bancarios.
+print "Digite o valor dos rendimentos de salário ou serviços: ";							//saida de dados
+$rendss = trim(fgets(STDIN));																//entrada de dados
 if ($rendss <= 8000){
 	$tribrendss = 0;
 }
 else if ($rendss >= 8000.01 && $rendss <= 24000){
-	$tribrendss = ($rendss*0.15); 																//Trib 15% sobre rendimentos de salario e serviços.
+	$tribrendss = ($rendss*0.15); 															//Trib 15% sobre rendimentos de salario e serviços.
 }
 else{
 	$tribrendss=($rendss*0.2); 																//Trib 20% sobre rendimentos de salario ou serviços.
 }
-print "Digite o valor anual dos outros rendimentos: ";
-$or = trim(fgets(STDIN));
+print "Digite o valor anual dos outros rendimentos: ";										//saida de dados
+$or = trim(fgets(STDIN));																	//entrada de dados
 $tribor = ($or * 0.1); 																		//Trib 10% do valor sobre outros rendimentos.
-print "Digite o total anual de serviços médicos adquiridos: ";
-$servm = trim(fgets(STDIN));
-print "Digite o total anual de serviços educacionais adquiridos: ";
-$serve = trim(fgets(STDIN));
-$ttrib = ($tribrendb + $tribrendss + $tribor);
-$abat = (($tribrendb+$tribrendss+$tribor) * 0.3);
-$servtotal = ($servm + $serve);
-print "\nTotal de rendimentos Bancarios: $rendb
-Total de rendimentos de salarios ou serviços: $rendss
-Total de outros rendimentos: $or
-Serviços medicos pagos: $servm
-Serviços educacionais pagos: $serve\n\n";
-print "Total de Impostos;
-R$	$tribrendb		(Sobre rendimentos bancarios)
-R$	$tribrendss		(Sobre salarios ou serviços)
-R$	$tribor		(Sobre outros rendimentos)
-R$	$ttrib		(Total)\n";
-print "\nMaximo a ser abatido: R$ $abat\n";
-print "\nTotal de valores possiveis de abater:
-R$	$servm		(Serviços medicos)
-R$	$serve		(Serviços educacionais)
-R$	$servtotal		(Total)\n";
+print "Digite o total anual de serviços médicos adquiridos: ";								//saida de dados
+$servm = trim(fgets(STDIN));																//entrada de dados
+print "Digite o total anual de serviços educacionais adquiridos: ";							//saida de dados
+$serve = trim(fgets(STDIN));																//entrada de dados
+$ttrib = ($tribrendb + $tribrendss + $tribor);												//atribuiçao
+$abat = (($tribrendb+$tribrendss+$tribor) * 0.3);											//atribuiçao
+$servtotal = ($servm + $serve);																//atribuiçao
+print "\nTotal de rendimentos Bancarios: $rendb";												
+print "\nTotal de rendimentos de salarios ou serviços: $rendss";
+print "\nTotal de outros rendimentos: $or";
+print "\nServiços medicos pagos: $servm";
+print "\nServiços educacionais pagos: $serve\n";
+print "\nTotal de Impostos";
+print "\nR$	$tribrendb		(Sobre rendimentos bancarios)";
+print "\nR$	$tribrendss		(Sobre salarios ou serviços)";
+print "\nR$	$tribor		(Sobre outros rendimentos)";
+print "\nR$	$ttrib		(Total)\n";
+print "\nMaximo a ser abatido: R$ $abat";
+print "\nTotal de valores possiveis de abater:";
+print "\nR$	$servm		(Serviços medicos)";
+print "\nR$	$serve		(Serviços educacionais)";
+print "\nR$	$servtotal		(Total)\n";
 if ($servtotal < $abat){
-	print"
-	Imposto total:
-	R$	$ttrib			(Imposto bruto)
-	R$	$servtotal		(Abatimentos)
-	R$	".($ttrib - $servtotal)."			(Total)"; 	
+	print "\nImposto total: ";
+	print "\nR$	$ttrib			(Imposto bruto)";
+	print "\nR$	$servtotal		(Abatimentos)";
+	print "\nR$	".($ttrib - $servtotal)."			(Total)";
 }
 else{
-	print"
-Imposto total:
-------------------------------------------------------------
-R$	$ttrib		(Imposto bruto)
-R$	$abat		(Abatimentos)
-R$	".($ttrib - $abat)."		(Total)";
+	print "\nImposto total:";
+	print "\n------------------------------------------------------------";
+	print "\nR$	$ttrib		(Imposto bruto)";
+	print "\nR$	$abat		(Abatimentos)";
+	print "\nR$	".($ttrib - $abat)."		(Total)";
 }
